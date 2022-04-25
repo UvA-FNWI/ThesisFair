61fe8ceb6dbd12298d9310cd911d45dce76bec17
Quinten Coltof <quintencoltof1@gmail.com>
2022-04-25 21:54:27 +0200

    Feat(event_service): Integrated db and graphql

--- event_service/src/index.js
+++ event_service/src/index.js
@@ -1,8 +1,14 @@
 import amqp from 'amqplib';
+import debugLib from 'debug';
 
+import { connect as connectDB } from './database.js';
+import graphql from './graphql.js';
+
+const debug = debugLib('event_service:messaging');
 const queue = 'API_events'
 
 const main = async () => {
+  await connectDB();
   const conn = await amqp.connect('amqp://rabbitmq');
   const channel = await conn.createChannel();
 
@@ -10,10 +16,13 @@ const main = async () => {
     durable: false,
   });
 
-  channel.consume(queue, (msg) => {
-    const reply = msg.content.toString() + ' - ack!';
-    console.log('Sending reply: ', reply);
-    channel.sendToQueue(msg.properties.replyTo, Buffer.from(reply), { correlationId: msg.properties.correlationId });
+  channel.consume(queue, async (msg) => {
+    debug('Recv corr: %ds, reply:To %s', msg.properties.correlationId, msg.properties.replyTo)
+
+    const query = msg.content.toString();
+    const reply = await graphql(query);
+
+    channel.sendToQueue(msg.properties.replyTo, Buffer.from(JSON.stringify(reply)), { correlationId: msg.properties.correlationId });
     channel.ack(msg);
   });
 }
