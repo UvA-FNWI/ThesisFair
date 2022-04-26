6fb5ae421b7d6f3a57421cc1379def68c7298873
Quinten Coltof <quintencoltof1@gmail.com>
2022-04-26 16:21:04 +0200

    Feat(event_service): Namespace mutations

--- event_service/src/graphql.js
+++ event_service/src/graphql.js
@@ -1,41 +1,76 @@
-import { graphql, buildSchema } from 'graphql';
-import { makeExecutableSchema } from '@graphql-tools/schema'
+import { graphql } from 'graphql';
+import { schemaComposer } from 'graphql-compose';
 import { readFileSync } from 'fs';
 
 import { Event } from './database.js';
 
-const resolvers = {
-    Query: {
-        event: (obj, args) => Event.findById(args.evid),
-    },
-    Mutation: {
-        createEvent: (obj, args) => {
-            console.log('Creating object', args);
-            return Event.create(args);
+schemaComposer.addTypeDefs(readFileSync('./schema.graphql').toString('utf8'));
+
+schemaComposer.Query.addNestedFields({
+    event: {
+        type: 'Event',
+        args: {
+            evid: 'ID!',
         },
-        updateEvent: (obj, args) => Event.findByIdAndUpdate(args.evid),
-        deleteEvent: (obj, args) => Event.findByIdAndDelete(args.evid),
-
-        addEntity: (obj, args) => Event.findByIdAndUpdate(args.evid, {
-            $push: {
-                entities: args.enid,
-            }
-        }),
-        delEntity: (obj, args) => Event.findByIdAndUpdate(args.evid, {
-            $pull: {
-                entities: args.enid,
-            }
-        }),
+        resolve: async (obj, args) => Event.findById(args.evid),
     },
-};
+});
 
-const schema = makeExecutableSchema({
-    typeDefs: readFileSync('./schema.graphql').toString('utf8'),
-    resolvers
-  });
+schemaComposer.Mutation.addNestedFields({
+    'event.create': {
+        type: 'Event',
+        args: {
+            enabled: 'Boolean',
+            name: 'String!',
+            description: 'String',
+            start: 'Date',
+            location: 'String',
+            studentSubmitDeadline: 'Date',
+            entities: '[ID!]'
+        },
+        resolve: async (obj, args) => await Event.create(args),
+    },
+    'event.update': {
+        type: 'Event',
+        args: {
+            evid: 'ID!',
+            enabled: 'Boolean',
+            name: 'String!',
+            description: 'String',
+            start: 'Date',
+            location: 'String',
+            studentSubmitDeadline: 'Date',
+            entities: '[ID!]'
+        },
+        resolve: (obj, args) => Event.findByIdAndUpdate(args.evid),
+    },
+    'event.delete': {
+        type: 'Event',
+        args: {
+            evid: 'ID!',
+        },
+        resolve: (obj, args) => Event.findByIdAndDelete(args.evid),
+    },
 
-const execute = (query, variableValues = {}) => {
-    return graphql({ schema, source: query, variableValues })
-}
+    'event.entity.add': {
+        type: 'Event',
+        args: {
+            evid: 'ID!',
+            enid: 'ID!',
+        },
+        resolve: (obj, args) => Event.findByIdAndUpdate(args.evid, { $push: { entities: args.enid } }),
+    },
+    'event.entity.del': {
+        type: 'Event',
+        args: {
+            evid: 'ID!',
+            enid: 'ID!',
+        },
+        resolve: (obj, args) => Event.findByIdAndUpdate(args.evid, { $pull: { entities: args.enid } }),
+    },
+})
 
+const schema = schemaComposer.buildSchema();
+
+const execute = (query, variableValues = {}) => graphql({ schema, source: query, variableValues });
 export default execute;
