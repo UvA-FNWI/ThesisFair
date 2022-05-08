86f028a3910e7eb19f7e3a644e438e40008c9848
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-08 15:17:13 +0200

    Chore: Removed all Enum querying code

--- libraries/graphql-query-builder/index.js
+++ libraries/graphql-query-builder/index.js
@@ -4,7 +4,7 @@ let url = "http://127.0.0.1:3000/graphql";
 let apiToken = null;
 
 export const setUrl = (newUrl) => {
-    url = newUrl;
+  url = newUrl;
 }
 
 export const login = async (username, password) => {
@@ -53,88 +53,51 @@ export const request = async (query, variables, shouldSucceed = true) => {
   return result.data;
 }
 
-class EnumType {
-  custom = true;
-
-  constructor(string) {
-    this.string = string;
-  }
-
-  toString() {
-    return this.string;
-  }
-}
-export const Enum = (name) => new EnumType(name);
-
 const serializeValue = (val) => {
-    switch (typeof val) {
-        case 'boolean':
-            return val.toString();
+  switch (typeof val) {
+    case 'boolean':
+      return val.toString();
 
-        case 'number':
-            return val.toString();
+    case 'number':
+      return val.toString();
 
-        case 'string':
-            return '"' + val + '"';
+    case 'string':
+      return '"' + val + '"';
 
-        case 'object':
-            if (val instanceof EnumType) {
-                return val.toString();
-            } else if (val instanceof Array) {
-                let result = '[';
-                for (const arr of val) {
-                    result += serializeValue(arr) + ',';
-                }
-                return result.substring(0, result.length - 1) + ']'
-            } else {
-                let result = '{';
-                for (const key in val) {
-                  result += `${key}:${serializeValue(val[key])},`
-                }
+    case 'object':
+      if (val instanceof Array) {
+        let result = '[';
+        for (const arr of val) {
+          result += serializeValue(arr) + ',';
+        }
+        return result.substring(0, result.length - 1) + ']'
+      } else {
+        let result = '{';
+        for (const key in val) {
+          result += `${key}:${serializeValue(val[key])},`
+        }
 
-                return result.substring(0, result.length - 1) + '}';
-            }
+        return result.substring(0, result.length - 1) + '}';
+      }
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 }
 
 export const dictToGraphql = (dict) => {
-    const result = serializeValue(dict);
-    return result.substring(1, result.length - 1);
-}
-
-export const dictToSimpleDict = (dict) => {
-    const res = {...dict};
-    for (const key in res) {
-      if (typeof res[key] !== 'object') {
-        continue;
-      }
-
-      if (res[key].custom) {
-        res[key] = res[key].toString();
-      } else if (Array.isArray(res[key])) {
-        res[key] = [...res[key]];
-        for (const index in res[key]) {
-            res[key][index] = dictToSimpleDict(res[key][index]);
-        }
-      } else {
-        res[key] = dictToSimpleDict(res[key]);
-      }
-    }
-
-    return res;
+  const result = serializeValue(dict);
+  return result.substring(1, result.length - 1);
 }
 
 export class Builder {
-    constructor(operation, name = '') {
-        this.operation = operation;
-        this.name = name;
-        this.queries = [];
-    }
+  constructor(operation, name = '') {
+    this.operation = operation;
+    this.name = name;
+    this.queries = [];
+  }
 
-    addQuery(query) {
-        this.queries.push(query);
-    }
+  addQuery(query) {
+    this.queries.push(query);
+  }
 }
--- test/test/graph-query-builder.js
+++ test/test/graph-query-builder.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { dictToGraphql, Enum } from '../../libraries/graphql-query-builder/index.js';
+import { dictToGraphql } from '../../libraries/graphql-query-builder/index.js';
 
 describe('lib.js', () => {
     it('Should properly transform a dict to a graphql named parameter string', () => {
@@ -7,14 +7,14 @@ describe('lib.js', () => {
             username: 'hi',
             password: 'ho',
             num: 1,
-            enum: Enum('test'),
+            enum: 'test',
             bool: true,
-            obj: { enum: Enum('test') },
-            arr: [{ enum: Enum('test') }, { enum: Enum('test') }],
+            obj: { enum: 'test' },
+            arr: [{ enum: 'test' }, { enum: 'test' }],
         }
 
         const result = dictToGraphql(dict);
 
-        expect(result).to.equal('username:"hi",password:"ho",num:1,enum:test,bool:true,obj:{enum:test},arr:[{enum:test},{enum:test}]');
+        expect(result).to.equal('username:"hi",password:"ho",num:1,enum:"test",bool:true,obj:{enum:"test"},arr:[{enum:"test"},{enum:"test"}]');
     });
 });
