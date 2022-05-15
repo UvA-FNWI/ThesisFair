43df6c1b9bbd79f9a226c679d6883150e2ef8c08
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-15 16:31:17 +0200

    Feat(project_service): Data structures

---
+++ project_service/src/database.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import debugLib from 'debug';
+
+const debug = debugLib('entity_service:database');
+
+export const connect = async () => {
+  const conStr = process.env.mongodbConStr || 'mongodb://mongodb/project_service';
+  const conn = await mongoose.connect(conStr);
+  debug(`Connected to database: ${conStr}`);
+  return conn;
+}
+
+const projectSchema = new mongoose.Schema({
+  enid: mongoose.Schema.ObjectId,
+  name: String,
+  description: String,
+  datanoseLink: String,
+});
+projectSchema.virtual('pid').get(function () { return this._id; }); // Create _id alias
+
+export const Project = mongoose.model('Project', projectSchema);
