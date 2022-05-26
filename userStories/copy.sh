5c194141f328b334338da199a473ac4f826f3da5
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-26 23:20:27 +0200

    Feat: Dockerize userStories to run in kubernetes

---
+++ userStories/copy.sh
@@ -0,0 +1,7 @@
+dependencies=("entity_service" "event_service" "project_service" "user_service" "vote_service")
+
+mkdir -p ./src/dbs
+
+for dep in "${dependencies[@]}"; do
+  cp "../$dep/src/database.js" "./src/dbs/$dep.js"
+done
