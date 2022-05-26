09ab40846c2f9f88b20b82f4cbc4e4c10e30c6cb
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-26 21:00:52 +0200

    Whoops

--- userStories/src/command/subordinate/representative/index.js
+++
@@ -1,9 +0,0 @@
-import subCli from '../subCli.js';
-
-import viewStudents from './viewStudents.js.js';
-import updateAccount from './updateAccount.js.js';
-
-const args = ['event', 'entity', 'representative'];
-const stories = [updateAccount, viewStudents];
-
-subCli('representative', args, stories);
