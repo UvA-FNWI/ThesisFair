02b9e70b5a490ed12c8a72f4b5e16f0db8042009
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-30 21:01:33 +0200

    Chore(kubernetes): Prevent re-installing of helm charts

--- server/initKubernetes/init.sh
+++ server/initKubernetes/init.sh
@@ -4,14 +4,24 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/a
 # RabbitMQ Custom Resource Definisons
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
 
+helmInstalled=$(helm list -A)
+
 # Prometheus for monitoring
-kubectl create namespace monitoring
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install prometheus prometheus-community/prometheus --namespace monitoring
+if [[ "$helmInstalled" == *"\nprometheus"* ]]; then
+  kubectl create namespace monitoring
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  helm install prometheus prometheus-community/prometheus --namespace monitoring
+else
+  echo "Prometheus already installed. Not re-installing"
+fi
 
 # Traefik for ingress routing
-helm repo add traefik https://helm.traefik.io/traefik
-helm install traefik traefik/traefik --set service.type=NodePort --set nodePort=true --set ports.web.nodePort=32080 --set ports.websecure.nodePort=32443 --set ports.traefik.expose=true
+if [[ "$helmInstalled" == *"\ntraefik"* ]]; then
+  helm repo add traefik https://helm.traefik.io/traefik
+  helm install traefik traefik/traefik --set service.type=NodePort --set nodePort=true --set ports.web.nodePort=32080 --set ports.websecure.nodePort=32443 --set ports.traefik.expose=true
+else
+  echo "Prometheus already installed. Not re-installing"
+fi
 
 ./build.sh
 kubectl apply -f ./build/
