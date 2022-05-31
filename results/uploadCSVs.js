b4f509a678bcfcbd62d64d3b0b3e07d3083a4b70
Quinten Coltof <quintencoltof1@gmail.com>
2022-05-31 13:06:22 +0200

    Feat: upload CSVs into results database

---
+++ results/uploadCSVs.js
@@ -0,0 +1,68 @@
+import fs from 'fs'
+import path from 'path'
+import { Command } from 'commander';
+import mongoose from 'mongoose';
+import { parse as csvParser } from 'csv-parse';
+
+const Result = mongoose.model('Result', new mongoose.Schema({
+  run: String,
+  time: Date,
+  proc: String,
+  event: String,
+  data: Object
+}));
+
+const uploadFile = (file, run) => {
+  return new Promise((resolve, reject) => {
+    const fileName = path.basename(file);
+    const parser = csvParser({ columns: true, skip_empty_lines: true, trim: true, relaxQuotes: true });
+
+    parser.on('readable', async () => {
+      let record;
+      while ((record = parser.read()) !== null) {
+        const timeKey = Object.keys(record)[0];
+        const time = record[timeKey];
+        delete record[timeKey];
+
+        await Result.create({
+          run: run,
+          time: time,
+          proc: fileName,
+          event: 'manualCSVImport',
+          data: record,
+        })
+      }
+    });
+
+    parser.on('error', reject);
+    parser.on('end', resolve);
+
+    const fileStream = fs.createReadStream(file, { encoding: 'utf-8' });
+    fileStream.pipe(parser);
+  });
+}
+
+const uploadDir = async (csvDir, db, run) => {
+  await mongoose.connect(db);
+  console.log('Connected to database');
+
+  for (const file of fs.readdirSync(csvDir)) {
+    console.log('Loading', file);
+    await uploadFile(path.join(csvDir, file), run);
+    console.log('Uploaded', file);
+  }
+
+  await mongoose.disconnect();
+}
+
+const main = () => {
+  const program = new Command();
+  program
+    .argument('<csvDir>', 'The directory of the csv files')
+    .argument('<db>', 'The mongodb database where to dump the results')
+    .argument('[run]', 'The run annotation to add to all the rows which are dumped')
+    .action(uploadDir);
+
+  program.parse();
+}
+main();
