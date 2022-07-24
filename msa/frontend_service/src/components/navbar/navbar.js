07dc4273115198d9dc79451d21b8c250d8e0f3cf
Quinten Coltof <quintencoltof1@gmail.com>
2022-07-24 09:20:41 +0200

    Fix(navbar): Preserve evid in the url

--- msa/frontend_service/src/components/navbar/navbar.js
+++ msa/frontend_service/src/components/navbar/navbar.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav'
 
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import dashboardIcon from 'bootstrap-icons/icons/person-circle.svg';
 import likesIcon from 'bootstrap-icons/icons/heart.svg';
+import organisationsIcon from 'bootstrap-icons/icons/building.svg';
 import logoutIcon from 'bootstrap-icons/icons/box-arrow-left.svg';
 
 import './navbar.scss';
 import api from '../../api';
 
-class CustomNavbar extends React.Component {
-  render() {
-    return (
-      // <Navbar className='custom-navbar'> // TODO: Make mobile friendly
-      //   <Container>
-      //     <Navbar.Brand href='/dashboard'>Thesis fair</Navbar.Brand>
-      //     <Navbar.Toggle />
-      //     <Navbar.Collapse>
-      //       <Navbar.Text>Test</Navbar.Text>
-      //     </Navbar.Collapse>
-      //   </Container>
-      // </Navbar>
-      <Nav className="d-md-block sidebar flex-column">
-        <Link className='logo' to='/dashboard'>
-          <img src="/images/uvalogo.svg" width="64" height="64" alt="UvA Logo" />
-        </Link>
-        <Nav.Item>
-            <Link to="/dashboard"><img src={dashboardIcon} alt='' /><span>Dashboard</span></Link>
-        </Nav.Item>
-        <Nav.Item>
-            <Link to="/likes"><img src={likesIcon} alt='' /><span>Likes</span></Link>
-        </Nav.Item>
+function CustomNavbar(props) {
+  const params = useParams();
 
-        <Nav.Item className='logout'>
-          <div onClick={api.user.logout}>
-            <img src={logoutIcon} alt='' />
-            <span>Logout</span>
-          </div>
-        </Nav.Item>
-      </Nav>
-    );
-  }
+  return (
+    // <Navbar className='custom-navbar'> // TODO: Make mobile friendly
+    //   <Container>
+    //     <Navbar.Brand href='/dashboard'>Thesis fair</Navbar.Brand>
+    //     <Navbar.Toggle />
+    //     <Navbar.Collapse>
+    //       <Navbar.Text>Test</Navbar.Text>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+    <Nav className="d-md-block sidebar flex-column">
+      <Link className='logo' to={`/${params.evid}/dashboard`}>
+        <img src="/images/uvalogo.svg" width="64" height="64" alt="UvA Logo" />
+      </Link>
+      <Nav.Item>
+        <Link to={`/${params.evid}/dashboard`}><img src={dashboardIcon} alt='' /><span>Dashboard</span></Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link to={`/${params.evid}/likes`}><img src={likesIcon} alt='' /><span>Likes</span></Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Link to={`/${params.evid}/organisations`}><img src={organisationsIcon} alt='' /><span>Orgs</span></Link>
+      </Nav.Item>
+
+      <Nav.Item className='logout'>
+        <div onClick={api.user.logout}>
+          <img src={logoutIcon} alt='' />
+          <span>Logout</span>
+        </div>
+      </Nav.Item>
+    </Nav>
+  );
 }
 
 export default CustomNavbar;
