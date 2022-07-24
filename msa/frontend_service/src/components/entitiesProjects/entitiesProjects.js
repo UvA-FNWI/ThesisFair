930c0d6f82939e7724290fdc1aafc72b18b8c543
Quinten Coltof <quintencoltof1@gmail.com>
2022-07-24 10:18:04 +0200

    Feat(entitiesProjects): Moved reusable code to a component for showing entities and their projects

---
+++ msa/frontend_service/src/components/entitiesProjects/entitiesProjects.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { Button, Accordion, Modal } from 'react-bootstrap';
+import api from '../../api';
+
+const httpRegex = /^https?:\/\//
+
+class EntitiesProjects extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sharedEntities: [],
+      popup: false,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await api.user.get(api.getApiTokenData().uid, { share: true }).exec();
+    this.setState({ sharedEntities: user.share });
+  }
+
+  renderProjectModal = () => {
+    const { enid, entityIndex, projectIndex } = this.state.popup;
+    const entity = this.props.entities[entityIndex];
+    const project = this.props.projects[enid][projectIndex];
+
+    return (
+      <Modal show={true} onHide={() => this.setState({ popup: false })} size='lg'>
+        <Modal.Header closeButton>
+          Project and company information
+        </Modal.Header>
+        <Modal.Body>
+          <div class='d-flex justify-content-between'>
+            <div>
+              <h1>{project.name}</h1>
+              <h4>About the project</h4>
+              {project.description}
+
+              <h4 className='mt-4'>Datanose link</h4>
+              <a href={project.datanoseLink} target='_blank' rel='noreferrer'>{project.datanoseLink}</a>
+            </div>
+
+            <div>
+              <h1>{entity.name}</h1>
+              <h4>About the company</h4>
+              {entity.description}
+
+              <h4 className='mt-4 mb-0'>Interested in this project?</h4>
+              <h6>Please contact them using one of the methods below</h6>
+              <ul>
+                {entity.contact.map((contact, i) => {
+                  let prefix = '';
+                  if (contact.type === 'email') {
+                    prefix = 'mailto:';
+                  } else if (contact.type === 'phonenumber') {
+                    prefix = 'tel:';
+                  } else if (contact.type === 'website') {
+                    if (!httpRegex.test(contact.content)) {
+                      prefix = 'https://';
+                    }
+                  } else {
+                    console.error('unkown contact type', contact.type);
+                    return null;
+                  }
+
+                  return (<li key={i}><a href={prefix + contact.content} target='_blank' rel='noreferrer'>{contact.content}</a></li>);
+                })}
+              </ul>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    )
+  }
+
+  setShare = async (enid, state) => {
+    await api.user.student.shareInfo(api.getApiTokenData().uid, enid, state).exec();
+
+    const sharedEntities = [...this.state.sharedEntities];
+    if (state) {
+      sharedEntities.push(enid);
+    } else {
+      sharedEntities.splice(sharedEntities.indexOf(enid), 1);
+    }
+    this.setState({ sharedEntities });
+  }
+
+  shareAll = async () => {
+    for (const { enid } of this.props.entities) {
+      await this.setShare(enid, true);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Accordion>
+          {this.props.entities.map((entity, entityIndex) => {
+            const shared = this.state.sharedEntities.includes(entity.enid);
+            return (
+              <Accordion.Item key={entityIndex} eventKey={entityIndex}>
+                <Accordion.Header>
+                  {entity.name}
+
+                  <div className='flex-grow-1 d-flex justify-content-end me-4'>
+                    <Button as='div' variant={shared ? 'outline-primary' : 'primary'} size='sm' onClick={(e) => { e.stopPropagation(); this.setShare(entity.enid, !shared) }}>{shared ? 'Unshare information' : 'Share information'}</Button>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>
+
+                  <Accordion>
+                    {this.props.projects[entity.enid] ? this.props.projects[entity.enid].map((project, projectIndex) => (
+                      <Accordion.Item key={projectIndex} eventKey={projectIndex}>
+                        <Accordion.Header>
+                          {project.name}
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          <p>{project.description}</p>
+
+                          <Button onClick={() => this.setState({ popup: { enid: entity.enid, entityIndex, projectIndex } })}>More info</Button>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    )) : null}
+                  </Accordion>
+
+                </Accordion.Body>
+              </Accordion.Item>
+            )
+          })}
+        </Accordion>
+
+        {this.state.popup ?
+          this.renderProjectModal()
+          : null}
+      </>
+    );
+  }
+}
+
+export default EntitiesProjects
--- msa/frontend_service/src/pages/student/Organisations.js
+++ msa/frontend_service/src/pages/student/Organisations.js
@@ -1,30 +1,25 @@
 import React from 'react';
-import { Container, Button, Accordion, Modal } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import api from '../../api';
+import EntitiesProjects from '../../components/entitiesProjects/entitiesProjects';
 
-const httpRegex = /^https?:\/\//
 
 class Organisations extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      sharedEntities: [],
       entities: [],
       projects: {},
-      popup: false,
     };
+    this.entitiesProjects = React.createRef();
   }
 
   async componentDidMount() {
-    const [user, event] = await Promise.all([
-      api.user.get(api.getApiTokenData().uid, { share: true }).exec(),
-      api.event.get(this.props.params.evid, { entities: true }).exec(),
-    ]);
-
+    const event = await api.event.get(this.props.params.evid, { entities: true }).exec();
     const entities = await api.entity.getMultiple(event.entities).exec();
-    this.setState({ entities, sharedEntities: user.share });
+    this.setState({ entities });
 
     const projects = {};
     for (const { enid } of entities) {
@@ -34,122 +29,14 @@ class Organisations extends React.Component {
     this.setState({ projects });
   }
 
-  renderProjectModal = () => {
-    const {enid, entityIndex, projectIndex } = this.state.popup;
-    const entity = this.state.entities[entityIndex];
-    const project = this.state.projects[enid][projectIndex];
-
-    return (
-      <Modal show={true} onHide={() => this.setState({ popup: false })} size='lg'>
-        <Modal.Header closeButton>
-          Project and company information
-        </Modal.Header>
-        <Modal.Body>
-          <div class='d-flex justify-content-between'>
-            <div>
-              <h1>{project.name}</h1>
-              <h4>About the project</h4>
-              {project.description}
-
-              <h4 className='mt-4'>Datanose link</h4>
-              <a href={project.datanoseLink} target='_blank' rel='noreferrer'>{project.datanoseLink}</a>
-            </div>
-
-            <div>
-              <h1>{entity.name}</h1>
-              <h4>About the company</h4>
-              {entity.description}
-
-              <h4 className='mt-4 mb-0'>Interested in this project?</h4>
-              <h6>Please contact them using one of the methods below</h6>
-              <ul>
-                {entity.contact.map((contact, i) => {
-                  let prefix = '';
-                  if (contact.type === 'email') {
-                    prefix = 'mailto:';
-                  } else if (contact.type === 'phonenumber') {
-                    prefix = 'tel:';
-                  } else if (contact.type === 'website') {
-                    if (!httpRegex.test(contact.content)) {
-                      prefix = 'https://';
-                    }
-                  } else {
-                    console.error('unkown contact type', contact.type);
-                    return null;
-                  }
-
-                  return (<li key={i}><a href={prefix + contact.content} target='_blank' rel='noreferrer'>{contact.content}</a></li>);
-                })}
-              </ul>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-    )
-  }
-
-  setShare = async (enid, state) => {
-    await api.user.student.shareInfo(api.getApiTokenData().uid, enid, state).exec();
-
-    const sharedEntities = [...this.state.sharedEntities];
-    if (state) {
-      sharedEntities.push(enid);
-    } else {
-      sharedEntities.splice(sharedEntities.indexOf(enid), 1);
-    }
-    this.setState({ sharedEntities });
-  }
-
-  shareAll = async () => {
-    for (const { enid } of this.state.entities) {
-      await this.setShare(enid, true);
-    }
-  }
-
   render() {
     return (
       <Container className='mt-2'>
         <div className='d-flex justify-content-between align-items-baseline mb-4'>
           <h1>Organisations</h1>
-          <Button onClick={this.shareAll}>Give all companies access</Button>
+          <Button onClick={() => this.entitiesProjects.current.shareAll()}>Give all companies access</Button>
         </div>
-        <Accordion>
-          {this.state.entities.map((entity, entityIndex) => {
-            const shared = this.state.sharedEntities.includes(entity.enid);
-            return (
-            <Accordion.Item key={entityIndex} eventKey={entityIndex}>
-              <Accordion.Header>
-                {entity.name}
-
-                <div className='flex-grow-1 d-flex justify-content-end me-4'>
-                  <Button variant={shared ? 'outline-primary' : 'primary'} size='sm' onClick={(e) => {e.stopPropagation(); this.setShare(entity.enid, !shared)}}>{shared ? 'Unshare information' : 'Share information'}</Button>
-                </div>
-              </Accordion.Header>
-              <Accordion.Body>
-
-                <Accordion>
-                  {this.state.projects[entity.enid] ? this.state.projects[entity.enid].map((project, projectIndex) => (
-                    <Accordion.Item key={projectIndex} eventKey={projectIndex}>
-                      <Accordion.Header>
-                        {project.name}
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        <p>{project.description}</p>
-
-                        <Button onClick={() => this.setState({ popup: { enid: entity.enid, entityIndex, projectIndex } })}>More info</Button>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  )) : null}
-                </Accordion>
-
-              </Accordion.Body>
-            </Accordion.Item>
-          )})}
-        </Accordion>
-
-        { this.state.popup ?
-          this.renderProjectModal()
-        : null }
+        <EntitiesProjects entities={this.state.entities} projects={this.state.projects} ref={this.entitiesProjects} />
       </Container>
     );
   }
