4e7b70d47f78bb6511ed8b87c747049420b9df3d
Quinten Coltof <quintencoltof1@gmail.com>
2022-09-27 13:43:36 +0200

    Feat(frontend): Admin generate or import schedule

--- msa/frontend_service/src/pages/admin/EventSchedule.js
+++ msa/frontend_service/src/pages/admin/EventSchedule.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Table, Form } from 'react-bootstrap';
+import { Container, Table, Form, Button } from 'react-bootstrap';
 import EditIcon from 'bootstrap-icons/icons/pencil-square.svg';
 import SaveIcon from 'bootstrap-icons/icons/check-lg.svg';
 import api from '../../api';
 
+const getStudentName = (student) => student.firstname || student.lastname ? (student.firstname || '') + ' ' + (student.lastname || '') : null;
+
 class TableRow extends React.Component {
   constructor(props) {
     super(props);
-
 
     this.state = {
       enid: this.props.appointment.enid,
@@ -16,6 +17,7 @@ class TableRow extends React.Component {
       entityName: this.props.appointment.entityName,
       uid: this.props.appointment.uid,
       name: this.props.appointment.name,
+      studentnumber: this.props.appointment.studentnumber,
       slot: this.props.appointment.slot,
 
       editing: false,
@@ -38,7 +40,8 @@ class TableRow extends React.Component {
     const entity = this.props.entities.find((entity) => entity.enid === this.state.enid);
     const student = this.props.students.find((student) => student.uid === this.state.uid);
     this.setState({
-      name: `${student.firstname} ${student.lastname}`,
+      name: getStudentName(student) || 'Unkown',
+      studentnumber: student.studentnumber,
       entityLocation: entity.location,
       entityName: entity.name,
       editing: false,
@@ -52,9 +55,10 @@ class TableRow extends React.Component {
           <td><Form.Control value={this.state.slot} onChange={(e) => this.setState({ slot: e.target.value })} /></td>
           <td>
             <Form.Select value={this.state.uid} onChange={(e) => this.setState({ uid: e.target.value })}>
-              {this.props.students.map((student) => <option key={student.uid} value={student.uid}>{student.firstname} {student.lastname}</option>)}
+              {this.props.students.map((student, i) => <option key={i} value={student.uid}>{getStudentName(student) || student.studentnumber}</option>)}
             </Form.Select>
           </td>
+          <td></td>
           <td>
             <Form.Select value={this.state.enid} onChange={(e) => this.setState({ enid: e.target.value })}>
               {this.props.entities.map((entity) => <option key={entity.enid} value={entity.enid}>{entity.name}</option>)}
@@ -69,6 +73,7 @@ class TableRow extends React.Component {
     return (
       <tr>
         <td>{this.state.slot}</td>
+        <td>{this.state.studentnumber}</td>
         <td>{this.state.name}</td>
         <td>{this.state.entityName}</td>
         <td>{this.state.entityLocation}</td>
@@ -87,34 +92,76 @@ class Schedule extends React.Component {
       schedule: [],
       entities: [],
       students: [],
+      loading: true,
+
+      uploadingCSV: false,
     };
   }
 
   async componentDidMount() {
-    const [event, schedule] = await Promise.all([
-      api.event.get(this.props.params.evid).exec(),
-      api.schedule.getAll(this.props.params.evid).exec(),
-    ]);
+    const event = await api.event.get(this.props.params.evid).exec();
+    if (!event) {
+      window.location = '/';
+      return;
+    }
 
     const [entities, students] = await Promise.all([
       api.entity.getMultiple(event.entities, { enid: true, name: true, location: true }).exec(),
-      api.user.getAll('student', { uid: true, firstname: true, lastname: true }).exec(),
+      api.user.getAll('student', { uid: true, firstname: true, lastname: true, studentnumber: true }).exec(),
     ]);
+
+    this.setState({ event, entities, students }, this.loadSchedule);
+  }
+
+  loadSchedule = async () => {
+    const schedule = await api.schedule.getAll(this.props.params.evid).exec();
+
     for (let i = 0; i < schedule.length; i++) {
-      const entity = entities.find((entity) => entity.enid === schedule[i].enid);
+      const entity = this.state.entities.find((entity) => entity.enid === schedule[i].enid);
       schedule[i].entityName = entity.name;
       schedule[i].entityLocation = entity.location;
 
-      const student = students.find((student) => student.uid === schedule[i].uid);
+      const student = this.state.students.find((student) => student.uid === schedule[i].uid);
       if (!student) {
         schedule[i].name = 'Error retrieving name';
         continue;
       }
 
-      schedule[i].name = student.firstname + ' ' + student.lastname;
+      schedule[i].name = getStudentName(student) || 'Unkown';
+      schedule[i].studentnumber = student.studentnumber;
     }
 
-    this.setState({ event, schedule, entities, students });
+    this.setState({ schedule, loading: false });
+  }
+
+  generateSchedule = async () => {
+    await api.schedule.generate(this.props.params.evid).exec();
+    await this.loadSchedule();
+  }
+
+  importSchedule = async () => {
+    if (this.state.uploadingCSV) { return; }
+
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = async () => {
+      const blob = input.files.item(0);
+      if (!blob) {
+        input.remove();
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        this.setState({ uploadingCSV: true });
+        await api.schedule.import(this.props.params.evid, reader.result).exec();
+        this.setState({ uploadingCSV: false });
+        input.remove();
+        await this.loadSchedule();
+      }
+      reader.readAsText(blob);
+    }
+    input.click();
   }
 
   render() {
@@ -123,24 +170,38 @@ class Schedule extends React.Component {
         <div className='mb-4'>
           <h1>Schedule on {new Date(this.state.event.start).toLocaleString('NL-nl').split(' ')[0]}</h1>
         </div>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Time</th>
-              <th>Student</th>
-              <th>Organisation</th>
-              <th>Place</th>
-              <th>Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.schedule.map((appointment) => (
-                <TableRow key={appointment.uid + appointment.slot} appointment={appointment} entities={this.state.entities} students={this.state.students} />
-              ))
-            }
-          </tbody>
-        </Table>
+        {this.state.loading ?
+          <p>Loading schedule</p>
+          :
+          this.state.schedule.length > 0 ?
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Time</th>
+                  <th>StudentID</th>
+                  <th>Student Name</th>
+                  <th>Organisation</th>
+                  <th>Place</th>
+                  <th>Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.schedule.map((appointment, i) => (
+                    <TableRow key={i} appointment={appointment} entities={this.state.entities} students={this.state.students} />
+                  ))
+                }
+              </tbody>
+            </Table>
+            :
+            <div>
+              <p>No schedule has been generated or imported yet.</p>
+              <Button onClick={this.generateSchedule}>Generate schedule</Button>
+              <span className='ps-2 pe-2'>OR</span>
+              <Button onClick={this.importSchedule}>{ this.state.uploadingCSV ? 'Importing CSV, this might take a while...' : 'Import schedule' }</Button>
+            </div>
+
+        }
       </Container>
     );
   }
