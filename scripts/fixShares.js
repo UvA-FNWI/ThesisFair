318f6c289ec18fe35e0cada73f384c5da400d683
Quinten Coltof <quintencoltof1@gmail.com>
2022-10-04 14:32:00 +0200

    Feat(scripts): Fix the shares manually afterwards

---
+++ scripts/fixShares.js
@@ -0,0 +1,38 @@
+import { connect as connectUser, disconnect as disconnectUser, Student } from '../msa/user_service/src/database.js';
+import { connect as connectVote, disconnect as disconnectVote, Vote } from '../msa/vote_service/src/database.js';
+import { connect as connectSchedule, disconnect as disconnectSchedule, Schedule } from '../msa/schedule_service/src/database.js';
+
+async function main() {
+  await connectUser('mongodb://localhost/user_service');
+  await connectVote('mongodb://localhost/vote_service');
+  await connectSchedule('mongodb://localhost/schedule_service');
+
+  console.log('Fixing based on votes...')
+  for (const { uid, evid, votes } of await Vote.find()) {
+    const student = await Student.findById(uid);
+    for (const { enid, pid } of votes) {
+      if (!student.share.includes(enid)) {
+        student.share.push(enid);
+      }
+    }
+
+    await student.save();
+  }
+
+
+  console.log('Fixing based on schedule...');
+  for (const { uid, enid } of await Schedule.find()) {
+    await Student.findByIdAndUpdate(uid, { $push: { share: enid } });
+  }
+
+  console.log('Done');
+
+  await disconnectUser();
+  await disconnectVote();
+  await disconnectSchedule();
+  return 0;
+}
+
+main().then((exitCode) => {
+  process.exitCode = exitCode;
+})
