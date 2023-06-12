51e15b50276e1f2e021abd9cc7a07f415c243c22
Ischa Abraham <contact@ischa.dev>
2023-06-12 21:46:27 +0200

    hardened security of project api

--- msa/project_service/src/permissions.js
+++ msa/project_service/src/permissions.js
@@ -1,13 +1,16 @@
-// TODO: Permission style taken over from Quinten, but not entirely secure.
-// Need to look up enid corresponding to pid from database instead of assuming
-// submitted enid (allows overwrite of someone else's pid otherwise)
-export const entityWriteAccess = (user, entity) => {
+import { Project } from './database.js'
+
+export function entityWriteAccess(user, entity) {
+  if (entity == null) {
+    throw new Error('BAD REQUEST invalid entity or project ID')
+  }
+
   switch (user.type) {
     case 'a':
       return true
 
     case 'r':
-      if (user.enid === entity.enid) return true
+      if (user.enid === entity.enid.toString()) return true
 
       throw new Error('UNAUTHORIZED representative has no access to entity')
 
@@ -19,4 +22,10 @@ export const entityWriteAccess = (user, entity) => {
   }
 }
 
-export const projectWriteAccess = entityWriteAccess
+export async function projectWriteAccess(user, project) {
+  if (project.pid) {
+    project = await Project.findById(project.pid)
+  }
+
+  return entityWriteAccess(user, project)
+}
