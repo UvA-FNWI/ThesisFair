f6f3d8359fa2916be609c2e526b94ac5c6ee17cc
Javernus <jakejongejans@gmail.com>
2023-06-25 17:18:24 +0200

    chore: Minor updates to stuff.

--- msa/frontend_service/src/components/projectEditor/projectEditor.js
+++ msa/frontend_service/src/components/projectEditor/projectEditor.js
@@ -26,7 +26,7 @@ class ProjectEditor extends React.Component {
       degrees: [],
       tags: [],
       email: '',
-      numberOfStudents: null,
+      numberOfStudents: '',
       hasBeenInteractedWith: {
         name: false,
         description: false,
@@ -42,6 +42,7 @@ class ProjectEditor extends React.Component {
 
     this.submit = this.submit.bind(this)
     this.cancel = this.cancel.bind(this)
+    this.deleteProject = this.deleteProject.bind(this)
     this.removeTag = this.removeTag.bind(this)
     this.handleTagCheck = this.handleTagCheck.bind(this)
     this.handleMasterCheck = this.handleMasterCheck.bind(this)
@@ -70,33 +71,17 @@ class ProjectEditor extends React.Component {
     // Make sure the page does not reload
     e.preventDefault()
 
-    // Get the `data-submit-type` attribute of the button that was clicked
-    // (either `submit`, `cancel` or `delete`)
-    const submitType = e.nativeEvent.submitter.getAttribute('data-submit-type')
-
-    switch (submitType) {
-      case 'submit':
-        if (!this.validate()) {
-          e.stopPropagation()
-          return
-        }
-
-        await this.updateProject(e)
-        break
-      case 'cancel':
-        this.cancel(e)
-        break
-      case 'delete':
-        this.deleteProject(e)
-        break
-      default:
-        throw new Error(`Unknown submit type: ${submitType}`)
+    if (!this.validate()) {
+      e.stopPropagation()
+      return
     }
 
-    return this.props.onClose()
+    await this.updateProject()
+
+    this.props.onClose()
   }
 
-  async updateProject(e) {
+  async updateProject() {
     const project = {
       enid: api.getApiTokenData().enid,
       name: this.state.name,
@@ -105,17 +90,21 @@ class ProjectEditor extends React.Component {
       tags: this.state.tags,
       attendance: this.state.attendance,
       environment: this.state.environment,
-      expectations: this.state.expectations,
       email: this.state.email,
-      numberOfStudents: this.state.numberOfStudents,
     }
+
+    if (this.state.numberOfStudents) project.numberOfStudents = Number(this.state.numberOfStudents)
+    if (this.state.expectations) project.expectations = this.state.expectations
 
     // TODO: handle errors and show to user
     if (this.props.params.pid) {
       await api.project.update({ ...project, pid: this.props.params.pid }).exec()
     } else {
+      console.log('creating project', project)
       await api.project.create({ ...project, evid: this.props.params.evid }).exec()
     }
+
+    console.log('project updated')
   }
 
   async deleteProject(e) {
@@ -197,29 +186,20 @@ class ProjectEditor extends React.Component {
     }
   }
 
-  validate = () => {
-    console.log(this.validation)
-    let bruh = Object.values(this.validation).every(f => f() === true)
-    console.log(bruh)
-    return bruh
-  }
+  validate = () => Object.values(this.validation).every(f => f() === true)
 
   validateEmail = email =>
-    String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      )
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+      email
+    )
 
   validation = {
     name: () => this.state.name.length > 0,
     description: () => this.state.description.length > 0,
     environment: () => this.state.environment.length > 0,
-
     degrees: () => this.state.degrees.length > 0,
     attendance: () => this.attendanceOptions.map(({ value }) => value).includes(this.state.attendance),
     tags: () => this.state.tags.length >= 1 && this.state.tags.length <= 3,
-
     expectations: () => true,
     email: () => this.validateEmail(this.state.email),
     numberOfStudents: () => {
@@ -533,9 +513,7 @@ class ProjectEditor extends React.Component {
           <Button
             variant='primary'
             type='submit'
-            data-submit-type='submit'
             onClick={e => {
-              e.preventDefault()
               // Set all hasBeenInteractedWith to true
               this.setState({
                 hasBeenInteractedWith: {
@@ -546,19 +524,23 @@ class ProjectEditor extends React.Component {
                   attendance: true,
                   degrees: true,
                   tags: true,
+                  email: true,
+                  numberOfStudents: true,
                 },
               })
+
+              this.submit(e)
             }}
           >
             Submit
           </Button>
 
-          <Button variant='secondary' type='cancel' data-submit-type='cancel' onClick={this.cancel}>
+          <Button variant='secondary' type='cancel' onClick={this.cancel}>
             Cancel
           </Button>
 
           {this.props.params.pid && (
-            <Button variant='secondary' type='cancel' data-submit-type='delete'>
+            <Button variant='secondary' type='cancel' onClick={this.deleteProject}>
               Delete project
             </Button>
           )}
