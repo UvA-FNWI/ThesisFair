f5c8860ee04717161c99f40b5448b6f69862fe32
Ischa Abraham <contact@ischa.dev>
2023-08-18 15:24:49 +0200

    add ability to request invoice

--- msa/payment_service/src/graphql.js
+++ msa/payment_service/src/graphql.js
@@ -11,17 +11,22 @@ schemaComposer.addTypeDefs(readFileSync('./src/schema.graphql').toString('utf8')
 async function getPayments(targets) {
   return await Payments.aggregate([
     { $addFields: {
+      // Create a new field that specifies priority of this payment. Paid
+      // payments are the most important and should always be selected, etc.
       sortStatus: {
         $switch: {
           branches: [
             { case: { $eq: ['$status', 'paid'] }, then: 0 },
-            { case: { $eq: ['$status', 'open'] }, then: 1 },
-            { case: { $eq: ['$status', 'failed'] }, then: 2 },
+            { case: { $eq: ['$status', 'invoice'] }, then: 1 },
+            { case: { $eq: ['$status', 'open'] }, then: 2 },
+            { case: { $eq: ['$status', 'failed'] }, then: 4 },
           ],
-          default: 3,
+          default: 5,
         },
       },
     }},
+    // Make sure to pick the payment by priority, and otherwise get the most
+    // recently created
     { $sort: { sortStatus: -1, _id: 1 } },
     { $group: {
       _id: '$target',
@@ -46,6 +51,40 @@ schemaComposer.Query.addNestedFields({
       return await getPayments(args.targets)
     }
   },
+  /* Note: this should really be a mutation, as it's not returning anything
+   * that might already be there, like paymentLink. Nevertheless, I opted for
+   * this just because it was easier */
+  invoice: {
+    type: 'Boolean', // True if an invoice request is created, false otherwise
+    args: {
+      target: 'String!',
+      amount: 'Int',
+    },
+    description: 'Request an invoice for this target, i.e. set the payment status to "invoice requested"',
+    resolve: async(_obj, args) => {
+      // Get the current payment status
+      const payments = await getPayments([args.target])
+      const payment = payments ? payments[0] : undefined
+
+      // If there is one that's already paid, don't create a new invoice request
+      if (payment?.status == 'paid') {
+        // throw new Error('This target has already been paid')
+        return false
+      }
+
+      if (payment?.status == 'invoice') {
+        return false
+      }
+
+      await Payments.create({
+        target: args.target,
+        amount: args.amount,
+        status: 'invoice',
+      })
+
+      return true
+    },
+  },
   paymentLink: {
     type: 'String',
     args: {
@@ -65,7 +104,7 @@ schemaComposer.Query.addNestedFields({
       }
 
       if (!args.amount) {
-        throw new Error("No link found -- specify amount to create one")
+        throw new Error('No link found -- specify amount to create one')
       }
 
       // Otherwise, if there are only failed, canceled or expired links
