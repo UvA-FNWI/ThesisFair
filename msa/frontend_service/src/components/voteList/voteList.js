8b5990602193304f0e885806132a96037862f93e
Ischa Abraham <contact@ischa.dev>
2023-09-30 22:03:10 +0200

    add basics of student list rendering

---
+++ msa/frontend_service/src/components/voteList/voteList.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { ViewportList } from 'react-viewport-list'
+
+import './voteListItem.scss'
+import './voteList.scss'
+
+function VoteList(props) {
+  const listRef = useRef(null)
+
+  return (
+    <div className='entity-list--red-border' style={props.style}>
+      <div className='entity-list' style={{ maxHeight: props.maxHeight }} ref={listRef}>
+        <ViewportList viewportRef={listRef} items={props.items} itemMinSize={48}>
+          {item => <VoteListItem key={item.uid || item.text} {...item} />}
+        </ViewportList>
+      </div>
+    </div>
+  )
+}
+
+function VoteListItem(props) {
+  switch (props?.type) {
+      case 'heading':
+        return (
+          <div>
+            <br />
+            <span className='d-flex justify-content-between'>
+              <h3 style={{ alignSelf: 'flex-end' }}>{props.text}</h3>
+            </span>
+            <hr style={{ marginTop: 0, marginBottom: '1.25em' }} />
+          </div>
+        )
+      case 'item':
+      default:
+        return (
+          <li className='entity-list-item'>
+            <div className='entity-list-item__header'>
+              <div className='entity-list-item__title'>
+                <p>{props.firstname} {props.lastname}</p>
+              </div>
+
+              {props.headerButtons && <div className='entity-list-item__buttons'>{props.headerButtons()}</div>}
+            </div>
+          </li>
+        )
+    }
+}
+
+// Export EntityList and EntityListItem as EntityList.Item
+VoteList.Item = VoteListItem
+
+export default VoteList
--- msa/frontend_service/src/pages/representative/Students.js
+++ msa/frontend_service/src/pages/representative/Students.js
@@ -1,79 +1,145 @@
-import downloadIcon from 'bootstrap-icons/icons/download.svg'
+import fuzzysort from 'fuzzysort'
 import React from 'react'
-import { Button, Container, Table } from 'react-bootstrap'
+import { Container, Form } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
 
-import api, { downloadCV } from '../../api'
+import api from '../../api'
+import VoteList from '../../components/voteList/voteList'
 
-class TableRow extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <tr>
-        <td>{this.props.user.studentnumber}</td>
-        <td>
-          {this.props.user.firstname} {this.props.user.lastname}
-        </td>
-        <td>{this.props.user.email}</td>
-        <td style={{ width: '1px', whiteSpace: 'nowrap' }}>
-          <Button
-            size='sm'
-            variant='outline-primary'
-            onClick={e => {
-              e.stopPropagation()
-              downloadCV(this.props.user.uid, `${this.props.user.firstname} ${this.props.user.lastname}`)
-            }}
-          >
-            <img src={downloadIcon} alt='download' />
-          </Button>
-        </td>
-      </tr>
-    )
-  }
-}
+import '../../styles/events.scss'
 
 class Students extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      users: [],
+      students: [], // List of items in the form of {firstname, lastname, ..., pids: [pid, ...]}
+      filteredStudents: [],
+      error: false,
+      filters: {
+        search: '',
+      },
     }
   }
 
-  async componentDidMount() {
-    const users = await api.user.getAll('student').exec()
-    this.setState({ users })
+  searchFilterFunction = (students, filters) => {
+    if (!filters.search) return students
+
+    return fuzzysort
+      .go(filters.search, this.state.students, { keys: ['firstname', 'lastname', 'email'], limit: 250, threshold: -5000 })
+      .map(res => res.obj)
   }
 
+  filterFunctions = [this.searchFilterFunction]
+
+  filter = (filters, allStudents = this.state.students) => {
+    const filteredStudents = this.filterFunctions.reduce(
+      (students, filterFunction) => filterFunction(students, filters),
+      allStudents
+    )
+
+    this.setState({ filteredStudents })
+  }
+
+  search = searchFilter => {
+    if (searchFilter === this.state.filters.search) return
+
+    if (!searchFilter) {
+      const filters = { ...this.state.filters, search: '' }
+
+      this.setState({ filters })
+      this.filter(filters)
+      return
+    }
+
+    const filters = { ...this.state.filters, search: searchFilter }
+
+    this.setState({ filters })
+
+    this.filter(filters)
+  }
+
+  async componentDidMount() {
+    let students
+    try {
+      students = await api.user.getAll('student').exec()
+    } catch (error) {
+      console.log(error)
+      this.setState({ error: true })
+    }
+
+    for (const student of students) {
+      student.pids = [Math.floor(Math.random() * 3)]
+    }
+
+    this.setState({ students })
+
+    this.filter(this.state.filters, students)
+  }
+
+  getNoResultText() {
+    console.log(this.state)
+    switch (true) {
+      // First case assumes there is at least one vote
+      case !this.state.error && this.state.students?.length === 0:
+        return 'Loading...'
+      case !this.state.error && this.state.filteredStudents?.length === 0:
+        return 'No votes found with the given filters and/or search term.'
+      default:
+        return 'Something went wrong while loading the votes.'
+    }
+  }
+
+  categorizeByProjects = students => {
+    const studentsByPid = new Map()
+
+    if (students?.length == 0)
+      return studentsByPid
+
+    for (const student of students) {
+      for (const pid of student.pids || []) {
+        if (studentsByPid.get(pid))
+          studentsByPid.get(pid).push(student)
+        else
+          studentsByPid.set(pid, [student])
+      }
+    }
+
+    return studentsByPid
+  }
+  
   render() {
     return (
-      <Container className='mt-2'>
-        <div className='mb-4'>
-          <h1>Students</h1>
-        </div>
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>StudentID</th>
-              <th>Full name</th>
-              <th>Email</th>
-              <th>CV</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.users.map(user => (
-              <TableRow key={user.uid} user={user} />
-            ))}
-          </tbody>
-        </Table>
+      <Container className='scrollable-page' style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+        <h1 className='events-page__header'>Students</h1>
+
+        <Form className='search-bar'>
+          <Form.Group className='mb-3' controlId='searchBar'>
+            <Form.Control type='text' placeholder='Search names' onInput={e => this.search(e.target.value)} />
+          </Form.Group>
+        </Form>
+
+      {this.state.filteredStudents?.length > 0
+        ? <VoteList
+            style={{ height: '1px', flexGrow: '1' }}
+            items={Array.from(this.categorizeByProjects(this.state.filteredStudents)).map(
+              ([pid, students]) => [
+                {type: 'heading', text: pid},
+                ...students,
+              ]
+            ).flat()}
+          />
+        : <p>{this.getNoResultText()}</p>
+      }
       </Container>
     )
   }
 }
 
-export default Students
+function StudentsWithParams(props) {
+  const params = useParams()
+
+  return <Students {...props} params={params} />
+}
+
+export default StudentsWithParams
